4635ed367b5ebb60568fb4824ba021471d83bc23
CanterburyEd23 <edhampton23@gmail.com>
2022-02-21 12:43:07 +0000

    create top level jsx elements

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import Sidebar from '../features/sidebar';
+import RedditContent from '../features/redditContent';
 
 const App = () => {
-  return (
-    <div className='appContainer'></div>
-  );
+    return (
+        <div className='reactContainer'>
+            <header>
+                <h1>Reddit - The Final Frontier</h1>
+            </header>
+            <main>
+                <Sidebar></Sidebar>
+                <RedditContent></RedditContent>
+            </main>
+            <footer>
+                <h4>Reddit - The Final Frontier</h4>
+                <hr />
+                <p>An astronomy themed Reddit app, built in React by Ed Hampton, 2022.</p>
+            </footer>            
+        </div>
+    );
 };
 
 export default App;
---
+++ src/features/redditContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RedditContent = () => {
+    return (
+        <div className="reactContainer">
+            <h2>RedditContent</h2>
+        </div>
+    );
+};
+
+export default RedditContent;
