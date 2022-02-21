deacf67a4fb95c3fd6ec57f1d4f5e96e5c3ecc05
CanterburyEd23 <edhampton23@gmail.com>
2022-02-21 16:19:26 +0000

    render Search Bar

--- src/features/SearchBar.js
+++ src/features/SearchBar.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const dispatch = useDispatch();
+
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatch(searchTerm);
+    };
+
     return (
         <section className="SearchBar">
-            <h3>Search Bar</h3>
-        </section>       
+            <form className="search" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleSearchTermChange}
+                    aria-label="Search Posts"
+                />
+                <button type="submit" onClick={handleSubmit} aria-label="Search">Go</button>
+            </form>            
+        </section>
     );
 };
 
