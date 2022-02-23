6638d161ce09935a501329a61b353e46be225e3a
CanterburyEd23 <edhampton23@gmail.com>
2022-02-23 13:47:26 +0000

    api setup for subreddit slice

--- src/app/subRedditSlice.js
+++ src/app/subRedditSlice.js
@@ -31,7 +31,7 @@ export const { startGetSubreddits, getSubredditsSuccess, getSubredditsFailed } =
 export const fetchSubreddits = () => async (dispatch) => {  //when we call fetchSubreddits, the following happens:
     try {
         dispatch(startGetSubreddits());  //We dispatch the startGetSubreddits action to the store.  This sets the "isLoading" property to "true", and "error" to false.
-        const subreddits = await getSubreddits();  //We teach our thunk that the variable "subreddits" = the result of calling "getSubreddits", and set it to await the response.  This will either be a success, or failure.
+        const subreddits = await getSubreddits();  //We teach our thunk that the variable "subreddits" = the result of calling "getSubreddits" (definied in the reddit.js file in the api folder), and set it to await the response.  This will either be a success, or failure.
         dispatch(getSubredditsSuccess(subreddits));  //It works, and we dispatch the getSubredditsSuccess action to the store, with a payload of "subreddits", as returned to us in the above line.
     } catch (error) {  //In the event the above doesn't work, we "catch" an error.    
         dispatch(getSubredditsFailed());  //We dispatch the "getSubredditsFailed" action to the store, which will set the "error" state to "true".
